4e2d1ddaa4b0ad63a1663d5d4cc7a397f2809975
Divyanatha_Dantu <divyanatha.dantu@gmail.com>
2023-02-18 18:00:53 -0500

    Fixed env path issue

--- MyUtils/Firebase.py
+++ MyUtils/Firebase.py
@@ -1,21 +1,27 @@
 import os
+import sys
 
 import firebase_admin
 from firebase_admin import credentials, firestore
+import os
 from dotenv import load_dotenv
-from pathlib import Path
-
-dotenv_path = Path('../local.env')
-load_dotenv(dotenv_path=dotenv_path)
 
 
 def initialize_firestore():
-    cred = credentials.Certificate(os.getenv('FIREBASE_ADMIN_SDK_PATH'))
+    load_dotenv()
+    cred_path = os.getenv('FIREBASE_PATH')
+    bundle_dir = getattr(sys, '_MEIPASS', os.path.abspath(os.path.dirname(__file__)))
+    path_to_dat = os.path.abspath(os.path.join(bundle_dir, cred_path))
+    # print("divi test 2", path_to_dat)
+    cred = credentials.Certificate(path_to_dat)
     firebase_admin.initialize_app(cred)
 
 
 def get_firestore_files(search_file_name):
-    if firestore is None:
+    try:
+        firestore.client()
+    except Exception as e:
+        print(e)
         initialize_firestore()
     db = firestore.client()
     docs = db.collection(u'sales_data_csv_files').where(u'file_name', u'>=', search_file_name). \
